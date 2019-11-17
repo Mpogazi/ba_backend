23004aaf888944f5469549178f3d71801166e3cb
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-11-17 00:22:17 -0500

    Added comments to setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Author: Fabrice B. Mpogazi
+#
+# Intent:
+#   This file should contain a script that 
+#   installs all the necessary software.
+#   Among them is redis, npm, mongodb, npm 
+#   packages. And also should help with 
+#   setting up environment variables for the
+#   different environment types.
+#   In addition, it should set up permissions
+#   to the different files especially in 
+#   the production stage of the process.
+
